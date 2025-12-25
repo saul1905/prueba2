50379934e9c336144a8e1d088c4076676bf2b24e
Jhorsh-code <jhorschclintonq@gmail.com>
2025-12-24 20:01:36 -0500

    Version Modificada Por Jhorsh Clinton 24/12/2025

---
+++ ProyCRUDCORE/Controllers/AlumnoController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyCRUDCORE.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ProyCRUDCORE/Program.cs
+++ ProyCRUDCORE/Program.cs
@@ -1,27 +1,23 @@
-using Microsoft.EntityFrameworkCore;
-using ProyCRUDCORE;
+﻿using Microsoft.EntityFrameworkCore;
 using ProyCRUDCORE.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
-//Agregando los Servicios necesarios
-builder.Services.AddDbContext<DbcrudcoreContext>(Options =>
-Options.UseSqlServer(builder.Configuration.GetConnectionString("cadenaSQL")));
+builder.Services.AddDbContext<DbcrudcoreContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("cadenaSQL"))
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
